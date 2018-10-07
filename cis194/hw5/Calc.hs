db0c304b11f04600037d99a6758c9be5fcd690d2
bert2 <shuairan@gmail.com>
2018-10-07 12:50:17 +0200

    finish exercise 4

--- cis194/hw5/Calc.hs
+++ cis194/hw5/Calc.hs
@@ -22,3 +22,27 @@ instance Expr ExprT where
     lit = Lit
     add = Add
     mul = Mul
+
+instance Expr Integer where
+    lit = id
+    add = (+)
+    mul = (*)
+
+instance Expr Bool where
+    lit = (>0)
+    add = (||)
+    mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+    lit                       = MinMax
+    add (MinMax x) (MinMax y) = lit (max x y)
+    mul (MinMax x) (MinMax y) = lit (min x y)
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+    lit                   = Mod7 . (`mod` 7)
+    add (Mod7 x) (Mod7 y) = lit (x + y)
+    mul (Mod7 x) (Mod7 y) = lit (x * y)
