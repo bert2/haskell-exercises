9a8da697d38bab0a3ebfd4d22854395829b2b0db
bert2 <shuairan@gmail.com>
2018-10-07 12:26:19 +0200

    finish exercise 3

--- cis194/hw5/Calc.hs
+++ cis194/hw5/Calc.hs
@@ -12,3 +12,13 @@ evalStr :: String -> Maybe Integer
 evalStr s = case parseExp Lit Add Mul s of
                 Just expr -> Just (eval expr)
                 _         -> Nothing
+
+class Expr a where
+    lit :: Integer -> a
+    add :: a -> a -> a
+    mul :: a -> a -> a
+
+instance Expr ExprT where
+    lit = Lit
+    add = Add
+    mul = Mul
