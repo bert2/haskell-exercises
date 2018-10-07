23f82fa19522c123671d2574bea7dbbce9357c85
bert2 <shuairan@gmail.com>
2018-10-07 11:20:14 +0200

    add type class examples

--- misc.hs
+++ misc.hs
@@ -16,8 +16,38 @@ sumEveryTwo []       = []
 sumEveryTwo (x:[])   = [x]
 sumEveryTwo (x:y:zs) = (x + y) : sumEveryTwo zs
 
+-- deriving a type class
+
 data Fruit = Apple | Orange deriving Show
 whichFruit f =
     case f of
         "apple" | False -> Apple
         "orange" -> Orange
+
+-- implementing a type class
+
+data Foo = F Int | G Char
+
+instance Eq Foo where
+    (F i1) == (F i2) = i1 == i2
+    (G c1) == (G c2) = c1 == c2
+    _      == _      = False
+
+-- our own type class
+
+class Listable a where
+    toList :: a -> [Int]
+instance Listable Int where
+    toList x = [x]
+instance Listable Bool where
+    toList True  = [1]
+    toList False = [0]
+instance (Listable a, Listable b) => Listable (a,b) where
+    toList (x,y) = toList x ++ toList y
+instance Listable a => Listable [a] where
+    toList = concatMap toList
+
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving Show
+instance Listable (Tree Int) where
+    toList Empty        = []
+    toList (Node x l r) = toList l ++ [x] ++ toList r
